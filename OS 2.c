b24b373caefd29c7c057d886ac491299acea8afe
skluiza <78211013+skluiza@users.noreply.github.com>
2024-05-07 18:58:00 +0530

    Update OS 2.c

--- OS 2.c
+++ OS 2.c
@@ -1,128 +1,39 @@
-
-#include <stdio.h>
-#define MAX 100
-
-int main()
-{
-    int AT[MAX],BT[MAX],CT[MAX],TAT[MAX],WT[MAX],avgTAT=0,avgWT=0,i,n;
-    printf("Enter the number of processes:");
-    scanf("%d",&n);
-    printf("Enter the arrival time and burst time:\n");
-    for(i=0;i<n;i++)
-    scanf("%d %d",&AT[i],&BT[i]);
-
-    CT[0]=BT[0];
-    for(i=1;i<n;i++)
-    CT[i]=CT[i-1]+BT[i];
-    for(i=0;i<n;i++)
-    {
-        TAT[i]=CT[i]-AT[i];
-        WT[i]=TAT[i]-BT[i];
-        avgTAT += TAT[i];
-        avgWT += WT[i];
-    }
-    printf("\nProcesses AT BT CT TAT WT\n");
-    for(i=0;i<n;i++)
-    printf("p[%d],%d\t,%d\t,%d\t,%d\t,%d\n",i+1,AT[i],BT[i],CT[i],TAT[i],WT[i]);
-    printf("\nAverage TAT=%d\n",avgTAT/n);
-    printf("\nAverage WT=%d\n",avgWT/n);
-    return 0;
-}
-
-#include <stdio.h>
-#define MAX 100
-int main()
-{
-    int AT[MAX],BT[MAX],CT[MAX],TAT[MAX],WT[MAX],avgTAT =0,avgWT =0,i,n;
-    printf("Enter number of processes:");
-    scanf("%d",&n);
-
-    printf("Enter arrival time and burst time:\n");
-    for(i=0;i<n;i++)
-    scanf("%d %d",&AT[i],&BT[i]);
-
-    CT[0]=BT[0];
-    for(i=1;i<n;i++)
-    CT[i]=CT[i-1]+BT[i];
-    for(i=0;i<n;i++)
-    {
-        TAT[i]=CT[i]-AT[i];
-        WT[i]=TAT[i]-BT[i];
-        avgWT +=WT[i];
-        avgTAT +=TAT[i];
-    }
-    printf("\nProcesses AT BT CT TAT WT\n");
-    for(i=0;i<n;i++)
-    scanf("p[%d],%d\t,%d\t,%d\t,%d\t,%d\n",i+1,AT[i],BT[i],CT[i],TAT[i],WT[i]);
-    printf("Avreage TAT=%d\n",avgTAT/n);
-        printf("Avreage WT=%d\n",avgWT/n);
-        return 0;
-
-}
-
-#include <stdio.h>
-#define MAX 100
-int main()
-{
-    int AT[MAX],BT[MAX],CT[MAX],TAT[MAX],WT[MAX],avgTAT=0,avgWT=0,i,n;
-    printf("Enter the processes:\n");
-    scanf("%d",&n);
-
-    printf("Enter the arrival time and burst time:\n");
-    for(i=0;i<n;i++)
-    scanf("%d %d",&AT[i],BT[i]);
-
-    CT[0]=BT[0];
-    for(i=1;i<n;i++)
-    CT[i]=CT[i-1]+BT[i];
-
-    for(i=0;i<n;i++)
-    {
-        TAT[i]=CT[i]-AT[i];
-        WT[i]=TAT[i]-BT[i];
-        avgWT+=WT[i];
-        avgTAT +=TAT[i];
-    }
-    printf("Processes AT BT CT TAT WT\n");
-    for(i=0;i<n;i++)
-    scanf("p[%d],%d\t,%d\t,%d\t,%d\t,%d\n",i+1,AT[i],BT[i],CT[i],TAT[i],WT[i]);
-    printf("Average TAT is %d",avgTAT /n);
-    printf("Avearge WT is %d",avgWT / n);
-    return 0;
-}
-
-*/
-
-#include<stdio.h>
-#define MAX 100
-
-int main(){
-    int AT[MAX], BT[MAX], CT[MAX], TAT[MAX], WT[MAX], avgTAT = 0, avgWT = 0, i, n;
-
-    printf("Enter Number of Processes: ");
-    scanf("%d", &n);
-
-    printf("Enter Arrival Time and Burst Time:\n");
-    for(i = 0; i < n; i++)
-        scanf("%d %d", &AT[i], &BT[i]);
-
-    CT[0] = BT[0];
-    for(i = 1; i < n; i++)
-        CT[i] = CT[i - 1] + BT[i];
-
-    for(i = 0; i < n; i++){
-        TAT[i] = CT[i] - AT[i];
-        WT[i] = TAT[i] - BT[i];
-        avgWT += WT[i];
-        avgTAT += TAT[i];
-    }
-
-    printf("\nProcess  AT  BT  CT  TAT  WT\n");
-    for(i = 0; i < n; i++)
-        printf("P[%d]\t%d\t%d\t%d\t%d\t%d\n", i + 1, AT[i], BT[i], CT[i], TAT[i], WT[i]);
-
-    printf("\nAverage TAT: %d\n", avgTAT / n);
-    printf("Average WT: %d\n", avgWT / n);
-
-    return 0;
-}
+#include<stdio.h> 
+#define MAX 100 
+int main(){ 
+int Arrival_time[MAX], Burst_time[MAX], Completion_time[MAX], 
+Turn_Around_time[MAX], Waiting_time[MAX], 
+Average_Turn_Around_time = 0, Average_Waiting_time = 0, i, j; 
+printf("Enter Process U Want: "); 
+scanf("%d",&j); 
+ 
+printf("Enter Arrival Time: "); 
+ for(i=0;i<j;i++){ 
+scanf("%d",&Arrival_time[i]); 
+ } 
+printf("Enter Burst Time: "); 
+ for(i=0;i<j;i++){ 
+scanf("%d",&Burst_time[i]); 
+ } 
+Completion_time[0] = Burst_time[0]; 
+ for(i=1;i<j;i++){ 
+Completion_time[i] = Completion_time[i-1] + Burst_time[i]; 
+ } 
+ for(i=0;i<j;i++){ 
+Turn_Around_time[i] = Completion_time[i] - Arrival_time[i]; 
+Waiting_time[i] = Turn_Around_time[i] - Burst_time[i]; 
+Average_Waiting_time += Waiting_time[i]; 
+Average_Turn_Around_time += Turn_Around_time[i]; 
+ } 
+printf("\nProcess Arrival(T) Burst(T) Completion(T) TurnAround(T) Waiting(T)"); 
+ for(i=0;i<j;i++){ 
+printf("\nP[%d]\t %d\t \t%d\t %d\t \t%d\t \t%d", i+1 
+,Arrival_time[i], Burst_time[i], Completion_time[i], 
+Turn_Around_time[i], Waiting_time[i]); 
+ } 
+printf("\n\nAverage Turn Around Time: %d", 
+Average_Turn_Around_time/j); 
+printf("\nAverage Waiting Time: %d\n", 
+Average_Waiting_time/j); 
+ return 0; 
+} 
